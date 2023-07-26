2244f974ffc16dc6b019269beaddf343e9edd64c
Wesley Finck <wesleyfinck@gmail.com>
2023-07-25 18:55:24 -0700

    include applet icon in build

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import checker from 'vite-plugin-checker';
+import { defineConfig, loadEnv } from 'vite';
+import { readFileSync } from 'fs';
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production'
 
   return isProduction ? {
     plugins: [
-      // {
-      //   name: 'copy-assets',
-      //   apply: 'build',
-      //   generateBundle() {
-      //     this.emitFile({
-      //       type: 'asset',
-      //       fileName: 'icon.png',
-      //       source: require('fs').readFileSync('icon.png'),
-      //     });
-      //   },
-      // },
+      {
+        name: 'copy-assets',
+        apply: 'build',
+        generateBundle() {
+          this.emitFile({
+            type: 'asset',
+            fileName: 'icon.png',
+            source: readFileSync('icon.png'),
+          });
+        },
+      },
     ],
     build: {
       lib: {
